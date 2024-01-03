e244e122fcd4536c2c5c94af905790d03ada80a4
Wenshuang Lu <wenshuang_lu@163.com>
2024-01-03 10:22:52 +0800

    feat(optimizer): add join-order-benchmark

--- sysbench/build.sh
+++ sysbench/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #MYSQL_DIR=/home/ubuntu/mysql_20180518
-MYSQL_DIR=/Users/wslu/work/mysql/mysql80-install
+MYSQL_DIR=/Users/wslu/work/mysql/mysql80-release
 CUR_DIR=`pwd`
+
+# Only MacOS need
+export MACOSX_DEPLOYMENT_TARGET=13.5
 
 #sudo apt-get install automake gcc
 cd ${CUR_DIR}/sysbench
